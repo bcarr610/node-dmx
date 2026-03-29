3138906456680ba1704347c1e70f061a746a5df9
bcarr610 <carrbrandon4@gmail.com>
2026-03-29 14:58:06 -0400

    Change all lights to use noteon instead of CC to fix issue with midi feedback loop

--- src/config.ts
+++ src/config.ts
@@ -33,27 +33,27 @@ const config: Config = {
       channels: [
         {
           name: "Brightness",
-          midi: [1, "CC", 1],
+          midi: [1, "NOTE", 24],
         },
         {
           name: "Red",
-          midi: [1, "CC", 2],
+          midi: [1, "NOTE", 25],
         },
         {
           name: "Green",
-          midi: [1, "CC", 3],
+          midi: [1, "NOTE", 26],
         },
         {
           name: "Blue",
-          midi: [1, "CC", 4],
+          midi: [1, "NOTE", 27],
         },
         {
           name: "Strobe",
-          midi: [1, "CC", 5],
+          midi: [1, "NOTE", 28],
         },
         {
           name: "Mode",
-          midi: [1, "CC", 6],
+          midi: [1, "NOTE", 29],
         },
       ],
     },
@@ -62,43 +62,43 @@ const config: Config = {
       channels: [
         {
           name: "Brightness",
-          midi: [2, "CC", 1],
+          midi: [2, "NOTE", 24],
         },
         {
           name: "Red",
-          midi: [2, "CC", 2],
+          midi: [2, "NOTE", 25],
         },
         {
           name: "Green",
-          midi: [2, "CC", 3],
+          midi: [2, "NOTE", 26],
         },
         {
           name: "Blue",
-          midi: [2, "CC", 4],
+          midi: [2, "NOTE", 27],
         },
         {
           name: "Strobe",
-          midi: [2, "CC", 5],
+          midi: [2, "NOTE", 28],
         },
         {
           name: "Mode 1",
-          midi: [2, "CC", 6],
+          midi: [2, "NOTE", 29],
         },
         {
           name: "Mode 2",
-          midi: [2, "CC", 7],
+          midi: [2, "NOTE", 30],
         },
         {
           name: "CH 6 Speed",
-          midi: [2, "CC", 8],
+          midi: [2, "NOTE", 31],
         },
         {
           name: "Outer LED Effect",
-          midi: [2, "CC", 9],
+          midi: [2, "NOTE", 32],
         },
         {
           name: "Outer LED Speed",
-          midi: [2, "CC", 10],
+          midi: [2, "NOTE", 33],
         },
       ],
     },
@@ -107,27 +107,27 @@ const config: Config = {
       channels: [
         {
           name: "Brightness",
-          midi: [3, "CC", 1],
+          midi: [3, "NOTE", 24],
         },
         {
           name: "Red",
-          midi: [3, "CC", 2],
+          midi: [3, "NOTE", 25],
         },
         {
           name: "Green",
-          midi: [3, "CC", 3],
+          midi: [3, "NOTE", 26],
         },
         {
           name: "Blue",
-          midi: [3, "CC", 4],
+          midi: [3, "NOTE", 27],
         },
         {
           name: "Strobe",
-          midi: [3, "CC", 5],
+          midi: [3, "NOTE", 28],
         },
         {
           name: "Mode",
-          midi: [3, "CC", 6],
+          midi: [3, "NOTE", 29],
         },
       ],
     },
@@ -136,35 +136,35 @@ const config: Config = {
       channels: [
         {
           name: "Brightness",
-          midi: [4, "CC", 1],
+          midi: [4, "NOTE", 24],
         },
         {
           name: "Red",
-          midi: [4, "CC", 2],
+          midi: [4, "NOTE", 25],
         },
         {
           name: "Green",
-          midi: [4, "CC", 3],
+          midi: [4, "NOTE", 26],
         },
         {
           name: "Blue",
-          midi: [4, "CC", 4],
+          midi: [4, "NOTE", 27],
         },
         {
           name: "White",
-          midi: [4, "CC", 5],
+          midi: [4, "NOTE", 28],
         },
         {
           name: "Strobe",
-          midi: [4, "CC", 6],
+          midi: [4, "NOTE", 29],
         },
         {
           name: "Fn",
-          midi: [4, "CC", 7],
+          midi: [4, "NOTE", 30],
         },
         {
           name: "Fn Speed",
-          midi: [4, "CC", 8],
+          midi: [4, "NOTE", 31],
         },
       ],
     },
@@ -173,35 +173,35 @@ const config: Config = {
       channels: [
         {
           name: "Brightness",
-          midi: [5, "CC", 1],
+          midi: [5, "NOTE", 24],
         },
         {
           name: "Red",
-          midi: [5, "CC", 2],
+          midi: [5, "NOTE", 25],
         },
         {
           name: "Green",
-          midi: [5, "CC", 3],
+          midi: [5, "NOTE", 26],
         },
         {
           name: "Blue",
-          midi: [5, "CC", 4],
+          midi: [5, "NOTE", 27],
         },
         {
           name: "White",
-          midi: [5, "CC", 5],
+          midi: [5, "NOTE", 28],
         },
         {
           name: "Strobe",
-          midi: [5, "CC", 6],
+          midi: [5, "NOTE", 29],
         },
         {
           name: "Fn",
-          midi: [5, "CC", 7],
+          midi: [5, "NOTE", 30],
         },
         {
           name: "Fn Speed",
-          midi: [5, "CC", 8],
+          midi: [5, "NOTE", 31],
         },
       ],
     },
